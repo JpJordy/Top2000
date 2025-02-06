72018e71b723046c700d42421c80bc83ba674425
MikeKorte12 <114648811+MikeKorte12@users.noreply.github.com>
2025-02-06 15:09:14 +0100

    Migrations

--- Top2000_API/Models/Lijst.cs
+++ Top2000_API/Models/Lijst.cs
@@ -6,11 +6,10 @@ namespace Top2000_API.Models
     {
         public int LijstId { get; set; }
         public int SongId { get; set; }
-        public int Top2000JaarId { get; set; }
+        public int Jaar { get; set; }
         public int Positie { get; set; }
 
         public Song Song { get; set; } = null!;
-        public Top2000Jaar Top2000Jaar { get; set; } = null!;
     }
 
 }

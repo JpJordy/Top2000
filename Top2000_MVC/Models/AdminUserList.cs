54959463fa513568224463acec4277b0e4768d6f
Jordy Profijt <0344188@student.rocvantwente.nl>
2025-03-12 11:18:23 +0100

    AdminController aangepast, data in models gezet

---
+++ Top2000_MVC/Models/AdminUserList.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace Top2000_MVC.Models
+{
+    public class AdminUserList
+    {
+        [JsonPropertyName("$values")]
+        public List<AdminUserWithRole> Values { get; set; }
+    }
+}
